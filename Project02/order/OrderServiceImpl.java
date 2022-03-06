908188777715ac51fc2a81a394d104787dc9ef4b
Dong82 <85945540+Chaos0103@users.noreply.github.com>
2022-03-06 13:55:25 +0900

    Lombok 적용

--- Project02/order/OrderServiceImpl.java
+++ Project02/order/OrderServiceImpl.java
@@ -3,20 +3,16 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor  // final 붙은 것들 생성자 만듬
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;  // 인터페이스에만 의존하도록 설계와 코드를 변경
-
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
