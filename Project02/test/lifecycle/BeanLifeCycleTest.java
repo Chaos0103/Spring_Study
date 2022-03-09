cec70de4d924adbdd5622d5ada9d049b6d2a70e5
Dong82 <85945540+Chaos0103@users.noreply.github.com>
2022-03-09 14:45:02 +0900

    Update BeanLifeCycleTest.java

--- Project02/test/lifecycle/BeanLifeCycleTest.java
+++ Project02/test/lifecycle/BeanLifeCycleTest.java
@@ -19,9 +19,9 @@ class BeanLifeCycleTest {
     @Configuration
     static class LifeCycleConfig {
 
-        @Bean(initMethod = "init", destroyMethod = "close")
+        @Bean
         public NetworkClient networkClient() {
-             NetworkClient networkClient = new NetworkClient();
+            NetworkClient networkClient = new NetworkClient();
             networkClient.setUrl("http://hello-spring.dev");
             return networkClient;
         }
