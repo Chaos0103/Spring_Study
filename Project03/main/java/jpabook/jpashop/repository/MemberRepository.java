a75b5d6a0817236df89f807f64cd00f9205b243c
Dong82 <85945540+Chaos0103@users.noreply.github.com>
2022-03-21 22:21:43 +0900

    Add files via upload

--- Project03/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ Project03/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -1,17 +1,17 @@
 package jpabook.jpashop.repository;
 
 import jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Repository
+@RequiredArgsConstructor
 public class MemberRepository {
 
-    @PersistenceContext
-    private EntityManager em;
+    private final EntityManager em;  // 스프링 부트에서 가능
 
     public void save(Member member) {
         em.persist(member);
